c97474ae156dbf9763d1b14e7d9c67d413878b55
Zachary Lawrence <zacharyclawrence@gmail.com>
2015-10-14 16:30:24 -0400

    Create url better

--- glass/src/main/java/com/example/zachary/test2/MainActivity.java
+++ glass/src/main/java/com/example/zachary/test2/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.KeyEvent;
 import android.widget.ImageView;
 
@@ -14,22 +15,48 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    /** Formats a Google static maps URL for the specified location and zoom level. */
-    private static String makeStaticMapsUrl(double latitude, double longitude, int zoom) {
-//        return String.format(STATIC_MAP_URL_TEMPLATE, latitude, longitude, zoom);
-        return "http://maps.googleapis.com/maps/api/staticmap"
-                + "?size=320x180"
-                + "&scale=2"
-                + "&maptype=roadmap"
-                + "&format=png"
-                // Maybe use a URL encoder for this part?
-                + "&markers=38.987595,-76.941287%7C38.987658,-76.940542%7C38.984457,-76.940107"
-                + "&path=color:0x0000ff%7Cweight:5%7C38.987595,-76.941287%7C38.987658,-76.940542%7C38.984457,-76.940107";
+    /* Formats a Google static maps URL for the specified location and zoom level. */
+    private static String makeStaticMapsUrl(List<Pair<Double, Double>> points) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("http://maps.googleapis.com/maps/api/staticmap");
+        builder.append("?size=320x180");
+        builder.append("&scale=2");
+        builder.append("&maptype=roadmap");
+        builder.append("&format=png");
+
+        // Add the markers to the map
+//        builder.append("&markers=");
+//        for (int i = 0; i < points.size(); i++) {
+//            Pair<Double, Double> point = points.get(i);
+//            builder.append(point.first + "," + point.second);
+//            if (i != (points.size() - 1)) {
+//                // Add the '|' character
+//                builder.append("%7C");
+//            }
+//        }
+
+        // Add the path to the map
+        builder.append("&path=color:0x0000ff%7Cweight:5%7C");
+        for (int i = 0; i < points.size(); i++) {
+            Pair<Double, Double> point = points.get(i);
+            builder.append(point.first + "," + point.second);
+            if (i != (points.size() - 1)) {
+                // Add the '|' character
+                builder.append("%7C");
+            }
+        }
+
+        System.out.println(builder.toString());
+
+        return builder.toString();
     }
 
     private ImageView view;
@@ -42,7 +69,7 @@ public class MainActivity extends Activity {
         view = new ImageView(this);
         setContentView(view);
 
-        loadImage(makeStaticMapsUrl(0, 0, 0));
+        loadImage(makeStaticMapsUrl(getDummyPath()));
     }
 
     /** Load the map asynchronously and populate the ImageView when it's loaded. */
@@ -79,12 +106,31 @@ public class MainActivity extends Activity {
                 loadImage("http://www.drodd.com/images12/arrow-clip-art36.png");
                 overviewMode = false;
             } else {
-                loadImage(makeStaticMapsUrl(0, 0, 0));
+                loadImage(makeStaticMapsUrl(getDummyPath()));
             }
 
             return true;
         }
 
         return super.onKeyDown(keycode, event);
+    }
+
+    // Provide dummy data to start testing
+    private List<Pair<Double, Double>> getDummyPath() {
+        ArrayList<Pair<Double, Double>> path = new ArrayList<>();
+
+        path.add(new Pair(38.987595, -76.941287));
+        path.add(new Pair(38.987658, -76.940542));
+        path.add(new Pair(38.984457, -76.940107));
+        path.add(new Pair(38.982812, -76.939213));
+        path.add(new Pair(38.982315, -76.939126));
+        path.add(new Pair(38.982284, -76.938809));
+        path.add(new Pair(38.981767, -76.938834));
+        path.add(new Pair(38.981772, -76.938961));
+        path.add(new Pair(38.980967, -76.938921));
+        path.add(new Pair(38.980949, -76.938759));
+        path.add(new Pair(38.980490, -76.938759));
+
+        return path;
     }
 }
