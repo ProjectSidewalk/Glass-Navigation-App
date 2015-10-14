b8e0ed1aa2d6acf4175fb00750f78c0fde3a641a
Zachary Lawrence <zacharyclawrence@gmail.com>
2015-10-14 16:34:02 -0400

    Reorder some code

--- glass/src/main/java/com/example/zachary/test2/MainActivity.java
+++ glass/src/main/java/com/example/zachary/test2/MainActivity.java
@@ -10,6 +10,8 @@ import android.util.Pair;
 import android.view.KeyEvent;
 import android.widget.ImageView;
 
+import com.google.android.glass.touchpad.GestureDetector;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -19,47 +21,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
-
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    /* Formats a Google static maps URL for the specified location and zoom level. */
-    private static String makeStaticMapsUrl(List<Pair<Double, Double>> points) {
-        StringBuilder builder = new StringBuilder();
-
-        builder.append("http://maps.googleapis.com/maps/api/staticmap");
-        builder.append("?size=320x180");
-        builder.append("&scale=2");
-        builder.append("&maptype=roadmap");
-        builder.append("&format=png");
-
-        // Add the markers to the map
-//        builder.append("&markers=");
-//        for (int i = 0; i < points.size(); i++) {
-//            Pair<Double, Double> point = points.get(i);
-//            builder.append(point.first + "," + point.second);
-//            if (i != (points.size() - 1)) {
-//                // Add the '|' character
-//                builder.append("%7C");
-//            }
-//        }
-
-        // Add the path to the map
-        builder.append("&path=color:0x0000ff%7Cweight:5%7C");
-        for (int i = 0; i < points.size(); i++) {
-            Pair<Double, Double> point = points.get(i);
-            builder.append(point.first + "," + point.second);
-            if (i != (points.size() - 1)) {
-                // Add the '|' character
-                builder.append("%7C");
-            }
-        }
-
-        System.out.println(builder.toString());
-
-        return builder.toString();
-    }
-
     private ImageView view;
+    private GestureDetector mGestureDetector;
     private boolean overviewMode = true;
 
     @Override
@@ -115,21 +80,58 @@ public class MainActivity extends Activity {
         return super.onKeyDown(keycode, event);
     }
 
+    // Formats a Google static maps URL
+    private static String makeStaticMapsUrl(List<Pair<Double, Double>> points) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("http://maps.googleapis.com/maps/api/staticmap");
+        builder.append("?size=320x180");
+        builder.append("&scale=2");
+        builder.append("&maptype=roadmap");
+        builder.append("&format=png");
+
+        // Add the markers to the map
+//        builder.append("&markers=");
+//        for (int i = 0; i < points.size(); i++) {
+//            Pair<Double, Double> point = points.get(i);
+//            builder.append(point.first + "," + point.second);
+//            if (i != (points.size() - 1)) {
+//                // Add the '|' character
+//                builder.append("%7C");
+//            }
+//        }
+
+        // Add the path to the map
+        builder.append("&path=color:0x0000ff%7Cweight:5%7C");
+        for (int i = 0; i < points.size(); i++) {
+            Pair<Double, Double> point = points.get(i);
+            builder.append(point.first + "," + point.second);
+            if (i != (points.size() - 1)) {
+                // Add the '|' character
+                builder.append("%7C");
+            }
+        }
+
+        System.out.println(builder.toString());
+
+        return builder.toString();
+    }
+
     // Provide dummy data to start testing
     private List<Pair<Double, Double>> getDummyPath() {
         ArrayList<Pair<Double, Double>> path = new ArrayList<>();
 
-        path.add(new Pair(38.987595, -76.941287));
-        path.add(new Pair(38.987658, -76.940542));
-        path.add(new Pair(38.984457, -76.940107));
-        path.add(new Pair(38.982812, -76.939213));
-        path.add(new Pair(38.982315, -76.939126));
-        path.add(new Pair(38.982284, -76.938809));
-        path.add(new Pair(38.981767, -76.938834));
-        path.add(new Pair(38.981772, -76.938961));
-        path.add(new Pair(38.980967, -76.938921));
-        path.add(new Pair(38.980949, -76.938759));
-        path.add(new Pair(38.980490, -76.938759));
+        path.add(new Pair<>(38.987595, -76.941287));
+        path.add(new Pair<>(38.987658, -76.940542));
+        path.add(new Pair<>(38.984457, -76.940107));
+        path.add(new Pair<>(38.982812, -76.939213));
+        path.add(new Pair<>(38.982315, -76.939126));
+        path.add(new Pair<>(38.982284, -76.938809));
+        path.add(new Pair<>(38.981767, -76.938834));
+        path.add(new Pair<>(38.981772, -76.938961));
+        path.add(new Pair<>(38.980967, -76.938921));
+        path.add(new Pair<>(38.980949, -76.938759));
+        path.add(new Pair<>(38.980490, -76.938759));
 
         return path;
     }
